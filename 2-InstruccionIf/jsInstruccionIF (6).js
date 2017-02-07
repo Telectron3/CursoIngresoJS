6f3791a1239094185923f1e021eac156747742e8
alumno <alumno>
2017-02-07 12:04:26 -0300

    Correccion de If ejercicio 6, Sentencias Switch ejercicio 1.

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,20 +2,23 @@ function Mostrar()
 {
 //tomo la edad  
 
-var Edad = document.getElementById("edad").value;
+	var Edad = document.getElementById("edad").value;
 
-if (Edad >= 18)
-{
-	alert ("Es mayor de edad");
-}
-else if(Edad >= 13 && Edad <= 17)
-{
-	alert(" Es adolecente"); 
-}
-else if (Edad <=12)
-{
-	alert ("Es un niño");
-}
+	if (Edad >= 18)
+	{
+		alert ("Es mayor de edad");
+	}
+	else 
+	{
+			if(Edad >= 13 && Edad <= 17)
+			{
+				alert(" Es adolecente"); 
+			}
+			else
+			{
+				alert ("Es un niño");
+			}
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,77 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño) 
+{
+	
+	case "Enero":
+	{
+		console.log("Enero");
+		alert ("Que comiences bien el año");
+		break;
+	}
 
+	
+	case "Febrero":
+	{
+		console.log("Febrero");
+		break;
 
+	}
+	case "Marzo":	
+	{
+		console.log("Marzo");
+		alert ("A clases");
+		break;
+	}
 
+	case "Abril":
+	{
+		console.log("Abril");
+		break;
+	}
+	case "Mayo":
+	{
+		console.log("Mayo");
+		break;
+	}
+	case "Junio":
+	{
+		console.log("Junio");
+		break;
+	}
+	case "Julio": 
+	{
+		console.log("Julio");
+		alert ("Se vienen las vacaciones");
+		break;
+	}
+	case "Agosto":
+	{
+		console.log("Agosto");
+		break;
+	}
+	case "Septiembre":
+	{
+		console.log("Septiembre");
+		break;
+	}
+
+	case "Octubre":
+	{
+		console.log("Octubre");
+		break;
+	}
+	case "Noviembre":
+	{
+		console.log("Noviembre");
+		break;
+	}
+		case "Diciembre":
+			{
+				console.log("Diciembre");
+				alert ("Felices Fiestas");
+				break;
+			}
+}
 }//FIN DE LA FUNCIÓN
