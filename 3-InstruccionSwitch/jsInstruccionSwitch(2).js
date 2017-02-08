647cc2244675b4adf34a59da183e7aa750dc258a
martin pagani <martin pagani>
2017-02-08 16:16:00 -0300

    Sentencia Switch  Ejercicio 2 y 3.

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,37 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch (mesDelAño)
+{
+	
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	case "Enero":
+	case "Febrero":
+	{
+		alert("Ya pasamos el frio, ahora calor!!");
+		break;
+	}
+
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	{
+		alert("Falta para el Invierno");
+		break;
+	}
+
+	case "Julio":
+	case "Agosto":
+	{
+		alert ("Abrigate hace frio");
+		break;
+	}
+}
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,9 +3,33 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+
 //alert (mesDelAño);
 	
-	
+switch (mesDelAño)
+{
+	case "Febrero":
+	{
+		alert ("Este mes no tiene más de 29 días ");
+		break;
+	}
+
+	case "Enero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	case "Julio":
+	case "Agosto":
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	{
+		alert ("Este mes tiene más de 30 días");
+		break;
+	}
+}	
 
 
 }//FIN DE LA FUNCIÓN
