bc34b185800b8892fc3776977f9a46fb8abf76e6
martin pagani <martin pagani>
2017-02-08 15:41:57 -0300

    Sentencia If Ejercicio 9 y 10.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var Nota = Math.floor((Math.random()*10) + 1);
+	console.log(Nota);
 	
+	if (Nota >= 9)
+		{
+			alert ("Exelente");
+
+		}
+		else
+		{
+			if (Nota >=4 && Nota < 9)
+			{
+				alert ("Aprobo");
+			}
+			else
+			{
+				alert ("Vamos, la proxima se puede");
+			}
+		}
 
 }//FIN DE LA FUNCIÓN
