0a21c8145710b76bbfe9fb37897ac415d9416525
martin pagani <martin pagani>
2017-02-13 20:59:08 -0300

    Tp

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -9,105 +9,216 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio () 
-{
-	var CantLamparas = parseInt(document.getElementById("Cantidad").value);
-	var Marca = document.getElementById("Marca").value;
-	var PrecioDescuento = parseFloat(document.getElementById("precioDescuento").value);
- 	var Lamparas = 35;
- 	var Lamparas = parseFloat(Lamparas);
- 	var Descuento;
- 	var Descuento = parseFloat(Descuento);
- 	
+ {
+ 	var CantLamparas = parseFloat(document.getElementById("Cantidad").value);
+ 	var Marca = document.getElementById("Marca").value;
+ 	var PrecioDescuento = parseFloat(document.getElementById("precioDescuento").value);
+  	var Lamparas = 35;
+  	var Lamparas = parseFloat(Lamparas);
+  	var Aumento;
+  	var Aumento = parseFloat(Aumento);
+  	var PrecioTotal = parseFloat(PrecioTotal);
+    var Descuento;
+  	var Descuento = parseFloat(Descuento);
+  	
+  if (PrecioDescuento > 120 && CantLamparas >= 6)
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.50;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
 
- 	if (CantLamparas >= 6 )
+          }  
+  	
+   else if (CantLamparas >= 6 )
+ 
+  		{
+  			CantLamparas = Lamparas * CantLamparas;
+  			Descuento = CantLamparas * 0.50;
+  			document.getElementById("precioDescuento").value = CantLamparas - Descuento;
+  			
+  		} 
 
- 		{
- 			CantLamparas = Lamparas * CantLamparas;
- 			Descuento = CantLamparas * 0.50;
- 			document.getElementById("precioDescuento").value = CantLamparas - Descuento;
- 		} 
+    else if (PrecioDescuento > 120 && CantLamparas == 5)
 
- 	else if (CantLamparas == 5)
- 	
- 	{
- 		switch (Marca)
- 		{
- 			case "ArgentinaLuz":
- 				{
+      switch(Marca)
+      {
+        case "ArgentinaLuz":
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.40;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+          break;
+        }
 
- 					CantLamparas = 5 * Lamparas;
- 					Descuento = CantLamparas * 0.40;
- 					document.getElementById("precioDescuento").value =  CantLamparas-Descuento;
- 					break; 
+        default:
+          {
+            CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.30;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+          break;
+          }
 
- 				}
+      }  
+        
+ 
+  	else if (CantLamparas == 5)
+  	
+  	{
+  		switch (Marca)
+  		{
+  			
+  			case "ArgentinaLuz":
+  				{
+ 
+  					CantLamparas = CantLamparas * Lamparas;
+  					Descuento = CantLamparas * 0.40;
+  					document.getElementById("precioDescuento").value =  CantLamparas-Descuento;
+  					break; 
+ 
+  				}
+ 
+  			default:
+  				{
+  					CantLamparas = CantLamparas * Lamparas;
+  					Descuento = CantLamparas * 0.30;
+  					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
+  					break;
+  				}
+  		}
+  		
+  	}
 
- 			default:
- 				{
- 					CantLamparas = 5 * Lamparas;
- 					Descuento = CantLamparas * 0.30;
- 					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
- 					break;
- 				}
- 		}		
- 	}
+    else if (PrecioDescuento > 120 && CantLamparas == 4)
+    {
+      switch(Marca)
+      {
+        case "ArgentinaLuz":
+        case "FelipeLamparas":
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.25;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+          break;
+        }
+        default:
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.20;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+          break;
+        }
+      }
+    }
 
- 	else if (CantLamparas == 4)
- 	
- 	{
- 		switch(Marca)
+ 
+  	else if (CantLamparas == 4)
+  	
+  	{
+  		switch(Marca)
+ 
+  		{
+  			case "ArgentinaLuz":
+  			case "FelipeLamparas":
+  				{
+  					CantLamparas = CantLamparas * Lamparas;
+  					Descuento = CantLamparas * 0.25;
+  					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
+  					break;
+  				}
+  			
+  			default:
+  				{
+  					CantLamparas = CantLamparas * Lamparas;
+  					Descuento = CantLamparas * 0.20;
+  					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
+  					break;
+  				}
 
- 		{
- 			case "ArgentinaLuz":
- 			case "FelipeLamparas":
- 				{
- 					CantLamparas = 4 * Lamparas;
- 					Descuento = CantLamparas * 0.25;
- 					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
- 					break;
- 				}
- 			default:
- 				{
- 					CantLamparas = 4 * Lamparas;
- 					Descuento = CantLamparas * 0.20;
- 					document.getElementById("precioDescuento").value = CantLamparas-Descuento;
- 					break;
- 				}
+ 		   }
+  	}
 
+    else if (PrecioDescuento > 120 && CantLamparas === 3) 
+
+    {
+      switch(Marca)
+      {
+        case "ArgentinaLuz":
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.15;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+        }
+        case "FelipeLamparas":
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.10;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+        }
+        default:
+        {
+          CantLamparas = Lamparas * CantLamparas;
+          Descuento = CantLamparas * 0.05;
+          PrecioTotal = CantLamparas - Descuento;
+          Aumento = PrecioTotal * 0.10;
+          document.getElementById("precioDescuento").value = Aumento + PrecioTotal;
+          alert ("Usted pago " + Aumento +" de IIBB");
+        }
+      }
+
+    }
+ 
+  	else if (CantLamparas == 3) 
+  	
+  		{
+  			switch(Marca)
+ 
+  			{
+  				case "ArgentinaLuz":
+  					{
+  						CantLamparas = CantLamparas * Lamparas;
+  						Descuento = CantLamparas * 0.15;
+ 						document.getElementById("precioDescuento").value = CantLamparas - Descuento;
+  						break;
+  					}
+  				
+  				case "FelipeLamparas":
+  					{
+  						CantLamparas = CantLamparas * Lamparas;
+  						Descuento = CantLamparas * 0.10;
+ 						document.getElementById("precioDescuento").value = CantLamparas - Descuento;
+  						break;
+  					}
+ 				
+ 				default:
+  					{
+  						CantLamparas = CantLamparas * Lamparas;
+  						Descuento = CantLamparas * 0.05;
+  						document.getElementById("precioDescuento").value = CantLamparas - Descuento;
+  						break;
+  					}
+  			}
  		}
  	}
 
- 	else if (CantLamparas == 3) 
- 	
- 	{
- 		switch(Marca)
-
- 		{
- 			case "ArgentinaLuz":
- 				{
- 					CantLamparas = 3 * Lamparas;
- 					Descuento = CantLamparas * 0.15;
- 					document.getElementById("precioDescuento").value = CantLamparas - Descuento;
- 					break;
- 				}
- 			case "FelipeLamparas":
- 				{
- 					CantLamparas = 3 * Lamparas;
- 					Descuento = CantLamparas * 0.10;
- 					document.getElementById("precioDescuento").value = CantLamparas - Descuento;
- 					break;
- 				}
- 			default:
- 				{
- 					CantLamparas = 3 * Lamparas;
- 					Descuento = CantLamparas * 0.05;
- 					document.getElementById("precioDescuento").value = CantLamparas - Descuento;
- 					break;
- 				}
- 		}
-
- 	}	
-
- 	
-
-}
+//GG m
